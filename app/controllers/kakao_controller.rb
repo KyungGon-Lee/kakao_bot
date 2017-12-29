d4276477b4c4eb48787a1b16e6c26be3e84ae5dd
Ubuntu <dlrudrhs3000@naver.com>
2017-12-29 02:12:53 +0000

    kakao lotto

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -12,6 +12,15 @@ class KakaoController < ApplicationController
     # 사용자가 보내준 텍스트를 그대로 다시 말하기
     user_message = params[:content]
 
+    # 로또
+    # 메뉴
+    # 다른 명령어가 들어왔을 때
+
+    if user_message = "로또"
+    lotto = (1..45).to_a.sample(6)
+    render json: lotto
+    end
+
     return_message = {
       :message => {
         :text => user_message
