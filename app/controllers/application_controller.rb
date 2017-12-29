ad806a384e69d38c9a0300a98af6d1f792290dd8
Ubuntu <dlrudrhs3000@naver.com>
2017-12-29 01:41:27 +0000

    csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception # 이걸 지워야 카카오톡 봇 작동함 csfr공격 차단하는것임
 end
