1e792836e92df2fe3210ed4e51928c8f07b47a06
Ubuntu <dlrudrhs3000@naver.com>
2017-12-29 01:27:37 +0000

    keyboard+message 기능추기

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -9,6 +9,17 @@ class KakaoController < ApplicationController
   end
 
   def message
+    # 사용자가 보내준 텍스트를 그대로 다시 말하기
+    user_message = params[:content]
+
+    return_message = {
+      :message => {
+        :text => user_message
+        },
+      # :keyboard = "" # 생략 가능
+    }
+
+    render json: return_message
 
   end
 end
